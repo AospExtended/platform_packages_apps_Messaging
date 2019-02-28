LineageOS/android_packages_apps_Messaging

Implement per conversation channels
Change-Id: I129d1eaab2e6be130238ff1dbabfb014bdcdb54c

src/com/android/messaging/ui/conversationsettings/PeopleAndOptionsFragment.java
@@ -18,12 +18,12 @@ package com.android.messaging.ui.conversationsettings;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
-import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
@@ -51,6 +51,8 @@ import com.android.messaging.ui.UIIntents;
 import com.android.messaging.ui.conversation.ConversationActivity;
 import com.android.messaging.util.Assert;
 
+import org.lineageos.messaging.util.NotifUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,8 +66,6 @@ public class PeopleAndOptionsFragment extends Fragment
     private PeopleListAdapter mPeopleListAdapter;
     private final Binding<PeopleAndOptionsData> mBinding =
             BindingBase.createBinding(this);
-
-    private static final int REQUEST_CODE_RINGTONE_PICKER = 1000;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -87,17 +87,6 @@ public class PeopleAndOptionsFragment extends Fragment
                 R.string.participant_list_title, true));
         mListView.setAdapter(compositeAdapter);
         return view;
-    }
-
-    @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_RINGTONE_PICKER) {
-            final Parcelable pick = data.getParcelableExtra(
-                    RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
-            final String pickedUri = pick == null ? "" : pick.toString();
-            mBinding.getData().setConversationNotificationSound(mBinding, pickedUri);
-        }
     }
 
     @Override
@@ -131,24 +120,20 @@ public class PeopleAndOptionsFragment extends Fragment
     }
 
     @Override
-    public void onOptionsItemViewClicked(final PeopleOptionsItemData item,
-            final boolean isChecked) {
+    public void onOptionsItemViewClicked(final PeopleOptionsItemData item) {
         switch (item.getItemId()) {
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_ENABLED:
-                mBinding.getData().enableConversationNotifications(mBinding, isChecked);
-                break;
-
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_SOUND_URI:
-                final Intent ringtonePickerIntent = UIIntents.get().getRingtonePickerIntent(
-                        getString(R.string.notification_sound_pref_title),
-                        item.getRingtoneUri(), Settings.System.DEFAULT_NOTIFICATION_URI,
-                        RingtoneManager.TYPE_NOTIFICATION);
-                startActivityForResult(ringtonePickerIntent, REQUEST_CODE_RINGTONE_PICKER);
-                break;
-
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_VIBRATION:
-                mBinding.getData().enableConversationNotificationVibration(mBinding,
-                        isChecked);
+            case PeopleOptionsItemData.SETTING_NOTIFICATION:
+                NotifUtils.createNotificationChannelGroup(getActivity(),
+                        NotifUtils.CONVERSATION_GROUP_NAME,
+                        R.string.notification_channel_messages_title);
+                NotifUtils.createNotificationChannel(getActivity(),
+                        mBinding.getData().getConversationId(),
+                        item.getOtherParticipant().getDisplayName(true),
+                        NotificationManager.IMPORTANCE_DEFAULT,
+                        NotifUtils.CONVERSATION_GROUP_NAME);
+                Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, getContext().getPackageName());
+                startActivity(intent);
                 break;
 
             case PeopleOptionsItemData.SETTING_BLOCKED:

src/org/lineageos/messaging/util/NotifUtils.java
@@ -17,17 +17,20 @@
 package org.lineageos.messaging.util;
 
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 
 public final class NotifUtils {
+    public static final String DEFAULT_CHANNEL_ID = "messaging_channel";
+    public static final String CONVERSATION_GROUP_NAME = "conversation_group";
 
     private NotifUtils() {
     }
 
     public static void createNotificationChannel(Context context, String id,
-            int titleResId, int priority) {
+            int titleResId, int priority, String group_id) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             return;
         }
@@ -40,6 +43,63 @@ public final class NotifUtils {
 
         String title = context.getString(titleResId);
         NotificationChannel newChannel = new NotificationChannel(id, title, priority);
+        if (group_id != null) {
+            newChannel.setGroup(group_id);
+        }
         manager.createNotificationChannel(newChannel);
+    }
+
+    public static void createNotificationChannel(Context context, String id,
+            String title, int priority, String group_id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        NotificationChannel existing = manager.getNotificationChannel(id);
+        if (existing != null) {
+            return;
+        }
+
+        NotificationChannel newChannel = new NotificationChannel(id, title, priority);
+        if (group_id != null) {
+            newChannel.setGroup(group_id);
+        }
+        manager.createNotificationChannel(newChannel);
+    }
+
+    public static void createNotificationChannelGroup(Context context, String id,
+            int titleResId) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        NotificationChannelGroup existing = manager.getNotificationChannelGroup(id);
+        if (existing != null) {
+            return;
+        }
+
+        String title = context.getString(titleResId);
+        NotificationChannelGroup newChannelGroup = new NotificationChannelGroup(id, title);
+        manager.createNotificationChannelGroup(newChannelGroup);
+    }
+
+    public static NotificationChannel getNotificationChannel(Context context, String id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return null;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        return manager.getNotificationChannel(id);
+    }
+
+    public static NotificationChannelGroup getNotificationChannelGroup(Context context, String id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return null;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        return manager.getNotificationChannelGroup(id);
     }
 }

AospExtended/platform_packages_apps_Messaging

src/com/android/messaging/ui/conversationsettings/PeopleAndOptionsFragment.java
@@ -18,12 +18,12 @@ package com.android.messaging.ui.conversationsettings;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Fragment;
+import android.app.NotificationManager;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
-import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.provider.Settings;
@@ -51,6 +51,8 @@ import com.android.messaging.ui.UIIntents;
 import com.android.messaging.ui.conversation.ConversationActivity;
 import com.android.messaging.util.Assert;
 
+import org.android.messaging.util.NotifUtils;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -64,8 +66,6 @@ public class PeopleAndOptionsFragment extends Fragment
     private PeopleListAdapter mPeopleListAdapter;
     private final Binding<PeopleAndOptionsData> mBinding =
             BindingBase.createBinding(this);
-
-    private static final int REQUEST_CODE_RINGTONE_PICKER = 1000;
 
     @Override
     public void onCreate(final Bundle savedInstanceState) {
@@ -87,17 +87,6 @@ public class PeopleAndOptionsFragment extends Fragment
                 R.string.participant_list_title, true));
         mListView.setAdapter(compositeAdapter);
         return view;
-    }
-
-    @Override
-    public void onActivityResult(final int requestCode, final int resultCode, final Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode == Activity.RESULT_OK && requestCode == REQUEST_CODE_RINGTONE_PICKER) {
-            final Parcelable pick = data.getParcelableExtra(
-                    RingtoneManager.EXTRA_RINGTONE_PICKED_URI);
-            final String pickedUri = pick == null ? "" : pick.toString();
-            mBinding.getData().setConversationNotificationSound(mBinding, pickedUri);
-        }
     }
 
     @Override
@@ -131,24 +120,20 @@ public class PeopleAndOptionsFragment extends Fragment
     }
 
     @Override
-    public void onOptionsItemViewClicked(final PeopleOptionsItemData item,
-            final boolean isChecked) {
+    public void onOptionsItemViewClicked(final PeopleOptionsItemData item) {
         switch (item.getItemId()) {
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_ENABLED:
-                mBinding.getData().enableConversationNotifications(mBinding, isChecked);
-                break;
-
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_SOUND_URI:
-                final Intent ringtonePickerIntent = UIIntents.get().getRingtonePickerIntent(
-                        getString(R.string.notification_sound_pref_title),
-                        item.getRingtoneUri(), Settings.System.DEFAULT_NOTIFICATION_URI,
-                        RingtoneManager.TYPE_NOTIFICATION);
-                startActivityForResult(ringtonePickerIntent, REQUEST_CODE_RINGTONE_PICKER);
-                break;
-
-            case PeopleOptionsItemData.SETTING_NOTIFICATION_VIBRATION:
-                mBinding.getData().enableConversationNotificationVibration(mBinding,
-                        isChecked);
+            case PeopleOptionsItemData.SETTING_NOTIFICATION:
+                NotifUtils.createNotificationChannelGroup(getActivity(),
+                        NotifUtils.CONVERSATION_GROUP_NAME,
+                        R.string.notification_channel_messages_title);
+                NotifUtils.createNotificationChannel(getActivity(),
+                        mBinding.getData().getConversationId(),
+                        item.getOtherParticipant().getDisplayName(true),
+                        NotificationManager.IMPORTANCE_DEFAULT,
+                        NotifUtils.CONVERSATION_GROUP_NAME);
+                Intent intent = new Intent(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+                intent.putExtra(Settings.EXTRA_APP_PACKAGE, getContext().getPackageName());
+                startActivity(intent);
                 break;
 
             case PeopleOptionsItemData.SETTING_BLOCKED:

src/org/android/messaging/util/NotifUtils.java
@@ -17,17 +17,20 @@
 package org.android.messaging.util;
 
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.os.Build;
 
 public final class NotifUtils {
+    public static final String DEFAULT_CHANNEL_ID = "messaging_channel";
+    public static final String CONVERSATION_GROUP_NAME = "conversation_group";
 
     private NotifUtils() {
     }
 
     public static void createNotificationChannel(Context context, String id,
-            int titleResId, int priority) {
+            int titleResId, int priority, String groupId) {
         if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
             return;
         }
@@ -40,6 +43,63 @@ public final class NotifUtils {
 
         String title = context.getString(titleResId);
         NotificationChannel newChannel = new NotificationChannel(id, title, priority);
+        if (groupId != null) {
+            newChannel.setGroup(groupId);
+        }
         manager.createNotificationChannel(newChannel);
+    }
+
+    public static void createNotificationChannel(Context context, String id,
+            String title, int priority, String groupId) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        NotificationChannel existing = manager.getNotificationChannel(id);
+        if (existing != null) {
+            return;
+        }
+
+        NotificationChannel newChannel = new NotificationChannel(id, title, priority);
+        if (groupId != null) {
+            newChannel.setGroup(groupId);
+        }
+        manager.createNotificationChannel(newChannel);
+    }
+
+    public static void createNotificationChannelGroup(Context context, String id,
+            int titleResId) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        NotificationChannelGroup existing = manager.getNotificationChannelGroup(id);
+        if (existing != null) {
+            return;
+        }
+
+        String title = context.getString(titleResId);
+        NotificationChannelGroup newChannelGroup = new NotificationChannelGroup(id, title);
+        manager.createNotificationChannelGroup(newChannelGroup);
+    }
+
+    public static NotificationChannel getNotificationChannel(Context context, String id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return null;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        return manager.getNotificationChannel(id);
+    }
+
+    public static NotificationChannelGroup getNotificationChannelGroup(Context context, String id) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            return null;
+        }
+
+        NotificationManager manager = context.getSystemService(NotificationManager.class);
+        return manager.getNotificationChannelGroup(id);
     }
 }